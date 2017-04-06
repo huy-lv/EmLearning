4bbecab696a4565b8a78fdd2d984bcbddaa5e14f
huylv177 <huylv177@gmail.com>
2017-04-06 21:16:09 +0700

    add stream mp3 file

--- app/src/main/java/com/hudati/emlearning/activity/ReadActivity.java
+++ app/src/main/java/com/hudati/emlearning/activity/ReadActivity.java
@@ -1,17 +1,18 @@
 package com.hudati.emlearning.activity;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 
 import com.hudati.emlearning.R;
 import com.hudati.emlearning.base.BaseToolbarActivity;
@@ -22,6 +23,7 @@ import com.koushikdutta.ion.Ion;
 import java.io.File;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 import static com.hudati.emlearning.util.Utils.INTENT_KEY_BOOK_NAME;
 import static com.hudati.emlearning.util.Utils.INTENT_KEY_BOOK_URL;
@@ -30,7 +32,8 @@ import static com.hudati.emlearning.util.Utils.INTENT_KEY_BOOK_URL;
  * Created by huylv on 28-Mar-17.
  */
 
-public class ReadActivity extends BaseToolbarActivity {
+public class ReadActivity extends BaseToolbarActivity implements MediaPlayer.OnCompletionListener, MediaPlayer.OnBufferingUpdateListener, SeekBar.OnSeekBarChangeListener {
+    private final Handler handler = new Handler();
     @BindView(R.id.read_wv)
     WebView read_wv;
     @BindView(R.id.read_pb)
@@ -38,8 +41,18 @@ public class ReadActivity extends BaseToolbarActivity {
     @BindView(R.id.toolbar_bt_download)
     ImageView toolbar_bt_download;
     String pdfUrl;
-    private String fileName;
+    @BindView(R.id.read_sb)
+    SeekBar read_sb;
+    @BindView(R.id.read_rewind)
+    ImageView read_previous;
+    @BindView(R.id.read_play)
+    ImageView read_play;
+    @BindView(R.id.read_forward)
+    ImageView read_forward;
     Context c;
+    private String fileName;
+    private MediaPlayer mediaPlayer;
+    private int mediaFileLengthInMilliseconds;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -68,12 +81,11 @@ public class ReadActivity extends BaseToolbarActivity {
                 Utils.showConfirmDialog(c, "Are you sure to download this file?", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-
-                        String path = Environment.getExternalStorageDirectory() +"/emlearning/"+ fileName;
+                        String path = Environment.getExternalStorageDirectory() + "/emlearning/" + fileName;
                         Ion.with(c).load(pdfUrl).write(new File(path)).setCallback(new FutureCallback<File>() {
                             @Override
                             public void onCompleted(Exception e, File result) {
-                                Utils.showInfoDialog(c,"Download done!");
+                                Utils.showInfoDialog(c, "Download done!");
                             }
                         });
                     }
@@ -81,11 +93,93 @@ public class ReadActivity extends BaseToolbarActivity {
 
             }
         });
+
+
+        //play media
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setOnBufferingUpdateListener(this);
+        mediaPlayer.setOnCompletionListener(this);
+        read_sb.setOnSeekBarChangeListener(this);
     }
 
+
+    @OnClick(R.id.read_rewind)
+    void rewind() {
+
+    }
+
+    @OnClick(R.id.read_forward)
+    void forward() {
+
+    }
+
+    @OnClick(R.id.read_play)
+    void play() {
+        try {
+            mediaPlayer.setDataSource("http://s1mp3.r9s70.vcdn.vn/3fe01ae2f4a61df844b7/978826610756853978?key=aXj5gOeuVFxpiBTP8c1bfA&expires=1491508470&filename=Yeu-5-Rhymastic.mp3"); // setup song from https://www.hrupin.com/wp-content/uploads/mp3/testsong_20_sec.mp3 URL to mediaplayer data source
+            mediaPlayer.prepare(); // you must call this method after setup the datasource in setDataSource method. After calling prepare() the instance of MediaPlayer starts load data from URL to internal buffer.
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mediaFileLengthInMilliseconds = mediaPlayer.getDuration(); // gets the song length in milliseconds from URL
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+            read_play.setImageResource(R.drawable.ic_pause_black_24dp);
+        } else {
+            mediaPlayer.pause();
+            read_play.setImageResource(R.drawable.ic_skill_play_iv);
+        }
+        primarySeekBarProgressUpdater();
+    }
+
+    private void primarySeekBarProgressUpdater() {
+        read_sb.setProgress((int) (((float) mediaPlayer.getCurrentPosition() / mediaFileLengthInMilliseconds) * 100)); // This math construction give a percentage of "was playing"/"song length"
+        if (mediaPlayer.isPlaying()) {
+            Runnable notification = new Runnable() {
+                public void run() {
+                    primarySeekBarProgressUpdater();
+                }
+            };
+            handler.postDelayed(notification, 1000);
+        }
+    }
 
     @Override
     protected int getLayoutId() {
         return R.layout.activity_read;
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mediaPlayer, int i) {
+        read_sb.setSecondaryProgress(i);
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+        read_play.setImageResource(R.drawable.ic_skill_play_iv);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        if (mediaPlayer.isPlaying()) {
+            int playPositionInMillisecconds = (mediaFileLengthInMilliseconds / 100) * read_sb.getProgress();
+            mediaPlayer.seekTo(playPositionInMillisecconds);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.pause();
     }
 }
